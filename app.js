ad96bd1328ea8f24492a244590d764016e986558
RockyRandhawa-hub <rockyrandhawa1808@gmail.com>
2025-07-27 18:46:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ import { otpVerificationRouter } from './routes/otpVerification.js';
 const app = express() ;
 app.use(cookieParser())
 app.use(cors({
-origin: "http://localhost:5173",
-    credentials:true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://reactpractce-rep.vercel.app"
+  ],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
